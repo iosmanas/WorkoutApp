5516f94bf1b6ab3457742876b54b8c92acbf4a00
Manas Duishoev <manasduishoev034@gmail.com>
2023-02-07 23:43:12 +0600

    Initial Commit

---
+++ WorkoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutApp
+//
+//  Created by new on 7/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
